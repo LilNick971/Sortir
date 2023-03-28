bb1962f70773ffd28767eebc68c8395e70aaf5cf
gollivier2022 <guillaume.ollivier2022@campus-eni.fr>
2023-03-28 16:03:28 +0200

    affichage sortie

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -2,17 +2,23 @@
 
 namespace App\Controller;
 
+use App\Repository\SortieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SortieController extends AbstractController
 {
-    #[Route('/sortie', name: 'app_sortie')]
-    public function index(): Response
+    #[Route('/liste', name: 'sortie_liste')]
+    public function listeSorties(
+        SortieRepository $sortieRepository,
+    ): Response
     {
-        return $this->render('sortie/index.html.twig', [
-            'controller_name' => 'SortieController',
+        $listeSortie =$sortieRepository->findBy([
+            'etat' => 2
         ]);
+        return $this->render('sortie/liste.html.twig',
+            compact('listeSortie'),
+        );
     }
 }
--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -2,7 +2,9 @@
 
 namespace App\Form;
 
+use App\Entity\Campus;
 use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
@@ -23,14 +25,12 @@ class RegistrationFormType extends AbstractType
             ->add('nom')
             ->add('telephone')
             ->add('email')
-//            ->add('agreeTerms', CheckboxType::class, [
-//                'mapped' => false,
-//                'constraints' => [
-//                    new IsTrue([
-//                        'message' => 'You should agree to our terms.',
-//                    ]),
-//                ],
-//            ])
+            ->add('campus', EntityType::class,
+            [
+                'class' => Campus::class,
+                'choice_label' => 'nom',
+                'placeholder' => ''
+            ])
             ->add('password', RepeatedType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
