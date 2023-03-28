29e90ab39573e3b7cc78be8c6482c3d6fc4ee193
LilNick971 <nicolashonorat@icloud.com>
2023-03-28 16:55:29 +0200

    Modifier son profil Afficher le profil des utilisateurs

---
+++ src/Controller/UserController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\RegistrationFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Config\Doctrine\Orm\EntityManagerConfig;
+
+#[Route('/user', name: 'user')]//Préfixe
+class UserController extends AbstractController
+{
+
+    #[Route('/modifier', name: '_modifier')]
+    public function modifier(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $user = $entityManager->find(User::class, $this->getUser()->getId());
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+            // do anything else you need here, like send an email
+
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('user/modifier.html.twig', [
+            'registrationForm' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/afficher/{user}', name: '_afficher')]
+    public function afficher(
+        User $user,
+        Request $request,
+        EntityManagerInterface $entityManager
+    ): Response
+    {
+        return $this->render('user/afficher.html.twig', compact("user"));
+    }
+
+}
